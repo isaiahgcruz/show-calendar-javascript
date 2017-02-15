040b035994b22882e23b3af879987f68cef14c45
Isaiah Cruz <isaiahgcruz@gmail.com>
2017-02-15 15:18:27 +0800

    Added episodes route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('./auth');
 const shows = require('./shows');
+const episodes = require('./episodes');
 
 const isAuthenticated = require('../middlewares/auth').isAuthenticated;
 
@@ -14,6 +15,7 @@ router.get('/', function(req, res, next) {
 router.use(auth);
 router.use(isAuthenticated);
 router.use('/shows/', shows);
+router.use('/', episodes);
 
 // here goes the protected routes
 router.get('/2', (req, res, next) => {
