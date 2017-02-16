fedc664763fb3bbf08b0a1c4b7f62dcca795e566
Isaiah Cruz <isaiahgcruz@gmail.com>
2017-02-16 18:56:50 +0800

    Install font-awesome

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,12 @@ module.exports = {
             scss: 'vue-style-loader!css-loader!sass-loader' // <style lang="scss">
           }
         }
-      }
+      },
+      { test: /\.svg$/, loader: 'url-loader?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]' },
+      { test: /\.woff$/, loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
+      { test: /\.woff2$/, loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
+      { test: /\.[ot]tf$/, loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
+      { test: /\.eot$/, loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' }
     ],
   },
   plugins: [
