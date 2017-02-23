e2c8cbda0dc4a2f7654d049b414661256606c587
Isaiah Cruz <isaiahgcruz@gmail.com>
2017-02-23 14:52:35 +0800

    Grouped episodes

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,8 @@ window._ = require('lodash');
 
 window.axios = require('axios');
 
+window.moment = require('moment');
+
 window.axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest',
 };
